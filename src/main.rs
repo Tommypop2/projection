e5e4342867c8350101d5882eeef5e460cf0df7d4
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-01-15 20:21:33 +0000

    feat: Allow for custom destinations

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,35 @@
-use std::{fs, path::PathBuf};
-use simple_home_dir;
 use clap::Parser;
 use fs_extra::dir::CopyOptions;
+use simple_home_dir;
+use std::{fs, path::PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 /// Easily scaffold projects from templates
 struct Args {
-    // #[arg(short, long)]
-    // name: String,
-
-    // /// Number of times to greet
-    // #[arg(short, long, default_value_t = 1)]
-    // count: u8,
     template_name: String,
+    destination: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
     // Get all directories
-    let dirs: Vec<PathBuf> = fs::read_dir(simple_home_dir::home_dir().expect("Cannot find home dir").join("projection"))
-        .expect("Unable to read directory")
-        .filter_map(|entry| {
-            let entry = entry.ok()?;
-            let path = entry.path();
-            if path.is_dir() {
-                Some(path)
-            } else {
-                None
-            }
-        })
-        .collect();
+    let dirs: Vec<PathBuf> = fs::read_dir(
+        simple_home_dir::home_dir()
+            .expect("Cannot find home dir")
+            .join("projection"),
+    )
+    .expect("Unable to read directory")
+    .filter_map(|entry| {
+        let entry = entry.ok()?;
+        let path = entry.path();
+        if path.is_dir() {
+            Some(path)
+        } else {
+            None
+        }
+    })
+    .collect();
     // Find the template
     let template_dir = dirs
         .iter()
@@ -39,7 +38,13 @@ fn main() {
     // Copy template to current working directory
 
     let cwd = std::env::current_dir().expect("Unable to get current working directory");
-    let new_dir = cwd.join(&args.template_name);
+    let new_dir = cwd.join({
+        if let Some(dest) = &args.destination {
+            dest
+        } else {
+            &args.template_name
+        }
+    });
     if new_dir.exists() {
         println!("Directory already exists");
         return;
@@ -47,6 +52,13 @@ fn main() {
     // Create new directory
     fs::create_dir(&new_dir).expect("Unable to create directory");
     // Copy contents of template into directory
-    fs_extra::dir::copy(template_dir, cwd, &CopyOptions::new())
-        .expect("Unable to copy template");
+    fs_extra::dir::copy(
+        template_dir,
+        new_dir,
+        &CopyOptions {
+            content_only: true,
+            ..Default::default()
+        },
+    )
+    .expect("Unable to copy template");
 }
